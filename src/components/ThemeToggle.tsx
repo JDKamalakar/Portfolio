dd5a7703f44d9f9cdb21cb97284ebc2f7be0608f
JDKamalakar <101495154+JDKamalakar@users.noreply.github.com>
2025-07-03 19:55:51 +0530

    Updated ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -183,7 +183,7 @@ const ThemeToggle = () => {
               ${isDarkActive ? 'scale-110' : ''}
               group-hover:rotate-[360deg] group-hover:scale-110`}
           />
-          <span className="text-sm font-medium hover:scale-[1.08]">Dark</span>
+          <span className="text-sm font-medium">Dark</span>
         </button>
       </div>
     </div>
